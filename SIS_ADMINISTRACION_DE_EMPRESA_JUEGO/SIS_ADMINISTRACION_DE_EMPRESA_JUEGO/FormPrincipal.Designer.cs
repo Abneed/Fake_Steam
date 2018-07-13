6332cfe4e8dcfd4ab2d21f3c5b0180651d4b0420
juliomendiola <41168587+juliomendiola@users.noreply.github.com>
2018-07-12 21:48:03 -0500

    solo es una prueba memo
    agerge solo un boton supongo que ahora tengo que ponerle en comit to

--- SIS_ADMINISTRACION_DE_EMPRESA_JUEGO/SIS_ADMINISTRACION_DE_EMPRESA_JUEGO/FormPrincipal.Designer.cs
+++ SIS_ADMINISTRACION_DE_EMPRESA_JUEGO/SIS_ADMINISTRACION_DE_EMPRESA_JUEGO/FormPrincipal.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnSalir = new System.Windows.Forms.Button();
+            this.btnPrueba = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSalir
@@ -41,12 +42,22 @@
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(123, 153);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba.TabIndex = 1;
+            this.btnPrueba.Text = "Prueba";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 396);
             this.ControlBox = false;
+            this.Controls.Add(this.btnPrueba);
             this.Controls.Add(this.btnSalir);
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -58,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnPrueba;
     }
 }
